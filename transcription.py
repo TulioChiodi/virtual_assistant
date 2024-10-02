b0017cf6170058ddb6cfdd934fe073050a68fbb3
tulio-chiodi <tulio.chiodi@omnilogic.ai>
2024-10-01 21:43:47 -0300

    removing deleted files and adding .env

--- transcription.py
+++
@@ -1,9 +0,0 @@
-import whisper
-
-# Initialize Whisper model (consider loading once)
-whisper_model = whisper.load_model('large')
-
-
-def transcribe_audio(filename='input_audio.wav'):
-    result = whisper_model.transcribe(filename)
-    return result['text']
